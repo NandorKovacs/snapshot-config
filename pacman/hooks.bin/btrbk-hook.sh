a54ea46e82788cf35c2b080bc39d1d8e39729045
Nandor Kovacs <nandor1kovacs@gmail.com>
2024-04-05 12:08:01 +0200

    no symlink

--- pacman/hooks.bin/btrbk-hook.sh
+++ pacman/hooks.bin/btrbk-hook.sh
@@ -1,1 +1,12 @@
-/etc/pacman.d/hooks.bin/btrbk-hook.sh
+#!/bin/sh
+
+Green='\033[0;32m'
+NC='\033[0m'
+Grey='\033[0;35m'
+echo -e "${Green}\n-------btrbk-------${Grey}"
+
+btrbk snapshot --format table --override=snapshot_create=always
+
+echo -e "${Green}-------done--------\n${NC}"
+
+
